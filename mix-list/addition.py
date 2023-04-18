c9337785a0574a521448bdd18beb8b86143ea02a
BigKatGalarraga <35146926+BigKatGalarraga@users.noreply.github.com>
2023-04-18 16:51:33 +0000

    updated repo

---
+++ mix-list/addition.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+c = input("Enter any number to be added: ")
+d = input("Enter any number to be added: ")
+
+# Converting inputted strings to integers
+c = int(c)
+d = int(d)
+
+sum = c + d
+print("The sum of", c, "and", d, "is:", sum)
+
---
+++ mix-list/fstring_example2.py
@@ -0,0 +1,6 @@
+name = "John Doe"
+age = 25
+
+# Example of an f-string
+print(f"My name is {name} and I'm {age} years old.")
+
